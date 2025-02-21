976d5b405829b46db6ce357728426d425453f318
DominatorStufs <dpdangergameryt@gmail.com>
2025-02-21 12:55:21 +0530

    Update Logo.jsx

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Logo() {
     return (
         <Link href="/">
-            <h1 className="select-none text-3xl font-bold">Zen<span className="text-primary">Pay</span></h1>
+            <h1 className="select-none text-3xl font-bold">Pay<span className="text-primary">Z</span></h1>
         </Link>
     )
 }
