935ac701f982e1e50d0b6b1ed4cb458b4dd0cffa
DominatorStufs <dpdangergameryt@gmail.com>
2025-02-21 13:18:57 +0530

    Update page.jsx

--- app/(user)/dashboard/pay/page.jsx
+++ app/(user)/dashboard/pay/page.jsx
@@ -1,8 +1,8 @@
 import PaymentForm from "@/app/(user)/_components/PaymentForm";
 
 export const metadata = {
-    title: "Pay",
-    description: "Transfer zen securely anywhere in the world.",
+    title: "PayZ",
+    description: "Transfer Z securely anywhere in the world.",
 }
 
 export default function Page() {
