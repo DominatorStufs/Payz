f368f9b7c78c10e3db03e137ed00604c6e2183dd
DominatorStufs <dpdangergameryt@gmail.com>
2025-02-21 13:03:53 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,11 +11,11 @@ export default function Footer() {
                     <Logo />
                     <span title="All System Normal" className="bg-background hover:bg-secondary transition select-none cursor-pointer flex items-center justify-center gap-1 text-xs rounded-full py-1 px-2 border">Normal<Computer className="h-3 w-3" /></span>
                 </div>
-                <p className="text-xs md:text-sm">Built using nextjs by <Link href="https://github.com/r2hu1" className="hover:text-primary hover:underline">r2hu1</Link><span className="text-primary">.</span></p>
+                <p className="text-xs md:text-sm">Built using nextjs by <Link href="https://github.com/DominatorStufs" className="hover:text-primary hover:underline">☠️DOMINATOR☠️</Link><span className="text-primary">.</span></p>
             </div>
             <ul className="grid">
-                <li className="-mb-1"><Link className="text-sm hover:underline hover:text-primary" href="https://rahul.eu.org">Hire Me</Link></li>
-                <li><Link className="text-sm hover:underline hover:text-primary" href="https://github.com/r2hu1/zenpay">Fix this page</Link></li>
+                <li className="-mb-1"><Link className="text-sm hover:underline hover:text-primary" href="https://wa.me/919968778724">Hire Me</Link></li>
+                <li><Link className="text-sm hover:underline hover:text-primary" href="https://github.com/DominatorStufs/Payz">Fix this page</Link></li>
             </ul>
             <p className="text-xs">©{new Date().getFullYear()} All rights reserved<span className="text-primary">.</span></p>
         </footer>
