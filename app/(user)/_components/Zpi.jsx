e8e6ecfbaa5d08344b48bd34a184a75020fce5af
DominatorStufs <dpdangergameryt@gmail.com>
2025-02-21 13:22:30 +0530

    Update Zpi.jsx

--- app/(user)/_components/Zpi.jsx
+++ app/(user)/_components/Zpi.jsx
@@ -5,14 +5,14 @@ import { Button } from "@/components/ui/button";
 import { Share2 } from "lucide-react";
 import { toast } from "sonner";
 
-export default function Zpi({ zpi, showshare = true }) {
+export default function payz({ payz, showshare = true }) {
     return (
         <div className="flex gap-2 items-center justify-center">
-            <span onClick={(e) => { navigator.clipboard.writeText(e.target.textContent); toast.success("ZPI copied!") }} className="bg-secondary border select-none hover:text-white px-4 py-2 text-sm rounded-full hover:bg-primary cursor-pointer transition">{zpi}</span>
+            <span onClick={(e) => { navigator.clipboard.writeText(e.target.textContent); toast.success("payz copied!") }} className="bg-secondary border select-none hover:text-white px-4 py-2 text-sm rounded-full hover:bg-primary cursor-pointer transition">{payz}</span>
             {showshare && (
                 <ModeToggle className="bg-transparent border-none"/>
             )}
-            {/* <Button size="icon" className="rounded-full" onClick={() => { navigator.share({ title: "ZenPay", url: "https://zenpay.vercel.app" }); }}><Share2 className="h-4 w-4" /></Button> */}
+            {/* <Button size="icon" className="rounded-full" onClick={() => { navigator.share({ title: "PayZ", url: "https://payz.vercel.app" }); }}><Share2 className="h-4 w-4" /></Button> */}
         </div>
     )
 }
